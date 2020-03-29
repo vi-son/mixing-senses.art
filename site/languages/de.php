40ea22d95967170160e77452825b608cf5e34b01
Guido Schmidt <git@guidoschmidt.cc>
2020-03-29 15:01:18 +0200

    ADD/site/languages: added German and English

---
+++ site/languages/de.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'code' => 'de',
+    'default' => true,
+    'direction' => 'ltr',
+    'locale' => [
+        'de_DE'
+    ],
+    'name' => 'Deutsch',
+    'translations' => [
+
+    ],
+    'url' => NULL
+];
---
+++ site/languages/en.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'code' => 'en',
+    'default' => false,
+    'direction' => 'ltr',
+    'locale' => [
+        'en_UK'
+    ],
+    'name' => 'English',
+    'translations' => [
+
+    ],
+    'url' => NULL
+];
