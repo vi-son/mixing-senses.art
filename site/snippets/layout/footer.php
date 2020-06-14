59cf13937fd88cc6486f4e260e2086b1de7553f9
Guido Schmidt <git@guidoschmidt.cc>
2020-06-14 18:02:51 +0200

    ADD/site/snippets/layout/head.php: add matomo tracking

--- site/snippets/layout/footer.php
+++ site/snippets/layout/footer.php
@@ -1,12 +1,12 @@
-<?php $pages = $site->children()->listed() ?>
+  <?php $pages = $site->children()->listed() ?>
 
-<section>
-  <nav class="footer-nav">
-    <?php foreach($pages as $p): ?>
-      <?= $p->title()->link() ?>
-    <?php endforeach ?>
-  </nav>
-</section>
+  <section>
+    <nav class="footer-nav">
+      <?php foreach($pages as $p): ?>
+        <?= $p->title()->link() ?>
+      <?php endforeach ?>
+    </nav>
+  </section>
 
   </body>
 </html>
--- site/snippets/layout/head.php
+++ site/snippets/layout/head.php
@@ -11,5 +11,25 @@
     <meta name="viewport"
           content="width=device-width,initial-scale=1.0">
     <?= css("assets/css/style.css") ?>
+
+    <!-- Matomo -->
+    <script type="text/javascript">
+      var _paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+      _paq.push(["setCookieDomain", "*.mixing-senses.art"]);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//matomo.guidoschmidt.cc/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="//matomo.guidoschmidt.cc/matomo.php?idsite=2&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Matomo Code -->
+
   </head>
   <body>
