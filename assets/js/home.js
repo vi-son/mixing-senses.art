dc1ba9e58b217e98794759581216d23b039ef011
Guido Schmidt <git@guidoschmidt.cc>
2020-06-14 13:58:50 +0200

    FIX/assets/js/home.js: change cookie behaviour

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,20 +1,25 @@
+function setCookie() {
+  const audioPermission = document.querySelector(".audio-permission");
+  Cookies.set("vi.son.audio", { useaudio: true }, { expires: 1 });
+  audioPermission.style.display = "none";
+}
+
 window.onload = function() {
   // defining cookie
-  var cookie = Cookies.getJSON('vi.son.audio');
-  console.log(cookie);
+  var cookie = Cookies.getJSON("vi.son.audio");
+  // Cookies.remove("vi.son.audio");
+  // console.log(cookie);
   const audioPermission = document.querySelector(".audio-permission");
 
   if (cookie) {
-    audioPermission.style.display = 'none';
+    audioPermission.style.display = "none";
     return;
   } else {
-    const allowButton = document.getElementById('allow-mic');
-    audioPermission.style.display = 'inline';
+    const allowMicButton = document.getElementById("button-mic");
+    const allowSongButton = document.getElementById("button-song");
+    audioPermission.style.display = "inline";
     // listen for on click
-    allowButton.addEventListener('click', function() {
-      Cookies.set('vi.son.audio', { useaudio: true }, { expires: 1 });
-      audioPermission.style.display = 'none';
-      switchToSong();
-    });
+    allowMicButton.addEventListener("click", setCookie);
+    allowSongButton.addEventListener("click", setCookie);
   }
- };
+};
