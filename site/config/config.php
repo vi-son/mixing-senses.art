14f5b3b93a83897a63e9fc578be9c9f0cd1836b8
Guido Schmidt <git@guidoschmidt.cc>
2020-06-14 13:59:10 +0200

    FIX/package.json + site/config/config.php: change site url

--- site/config/config.php
+++ site/config/config.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 }
     
 return [
-    'url' => 'http://192.168.2.20:8888',
+    'url' => 'http://127.0.0.1:8888',
     'debug' => true,
     'languages' => true,
     'api' => [
