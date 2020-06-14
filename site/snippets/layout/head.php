0a0f4fbbada9934ba734a943a63ad649e8864c27
Guido Schmidt <git@guidoschmidt.cc>
2020-06-14 15:06:28 +0200

    ADD/assets/svg: add favicon

--- site/snippets/layout/head.php
+++ site/snippets/layout/head.php
@@ -3,6 +3,11 @@
   <head>
     <meta charset="utf-8"/>
     <title><?= $page->title() ?></title>
+    <link
+      rel="icon"
+      type="image/svg+xml"
+      href="<?= $kirby->url("assets") ?>/svg/favicon.svg"
+      sizes="any">
     <meta name="viewport"
           content="width=device-width,initial-scale=1.0">
     <?= css("assets/css/style.css") ?>
