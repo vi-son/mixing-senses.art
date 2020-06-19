37a30f261986552dc762b0775a0db363204b0afa
Guido Schmidt <git@guidoschmidt.cc>
2020-06-20 00:31:31 +0200

    ADD/site/blueprints/site.yml: introduce footer secrets to advertise kirby

--- site/snippets/layout/footer.php
+++ site/snippets/layout/footer.php
@@ -1,11 +1,19 @@
   <?php $pages = $site->children()->listed() ?>
 
-  <section>
+  <section class="footer-block">
     <nav class="footer-nav">
       <?php foreach($pages as $p): ?>
         <?= $p->title()->link() ?>
       <?php endforeach ?>
     </nav>
+    <div class="support">
+      <small>
+        <?=  $site->secrets()->toStructure()->shuffle()->first()->text()->kt() ?>
+      </small>
+      <a href="https://getkirby.com" target="_blank">
+        <?= svg("assets/svg/kirby-logo.svg") ?>
+      </a>
+    </div>
   </section>
 
   </body>
